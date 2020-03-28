de6d071e64378470f49bd1689f0244413075bd37
Nizar Triki <triki.nizar@gmail.com>
2020-03-28 20:12:26 +0100

    Add mailer -fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
     def create
       @contact = Contact.new(params[:contact])
       @contact.request = request
-      if @contact.deliver
+      if @contact.deliver_now
         flash.now[:success] = "Message envoyé!"
       else
         flash.now[:error] = "On n'a pas pu envoyer votre message !"
