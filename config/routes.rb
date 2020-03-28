0dddf60846a9d8a4c5e93c130440dd3b6c39bed6
Nizar Triki <triki.nizar@gmail.com>
2020-03-28 17:27:04 +0100

    Add mailer -fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#index'
   post 'static/thankyou' 
+  get 'static/thankyou' 
 end
