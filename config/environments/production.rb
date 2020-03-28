74d43ce9e64a301114e7f35daf55d5be42f9db44
Nizar Triki <triki.nizar@gmail.com>
2020-03-29 00:19:06 +0100

    Add mailer -fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,15 +98,21 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'artemispharma.heroku.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
+  class IncomingMailsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def create
+    Rails.logger.log params[:envelope][:to] # print the to field to the logs
+    Rails.logger.log params[:subject] # print the subject to the logs
+    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
+    Rails.logger.log params[:html] # print the html decoded body to the logs if present
+    Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
+
+    # Do some other stuff with the mail message
+
+    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+  end
+end
 
 
 end
