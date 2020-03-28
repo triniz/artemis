d0485da5f038471aea6768c9bfa90dc69ef3bb86
Nizar Triki <triki.nizar@gmail.com>
2020-03-28 13:53:11 +0100

    Add mailer

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,14 @@
 class StaticController < ApplicationController
     def index
     end
+    def thankyou
+        @name = params[:name]
+        @email = params[:email]
+        @subject = params[:subject]
+        @message = params[:message]
+        ActionMailer::Base.mail(from: @email,
+        to: 'triki.nizar@gmail.com',
+        subject: "Artemispharma.net: un nouveau message de #{@name}",
+        body: "#{@subject}<br>#{@message}").deliver_now
+    end
 end
