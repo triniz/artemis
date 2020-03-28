a0b9aff7007c6f1d245b5898052aa033bbde2f1b
Nizar Triki <triki.nizar@gmail.com>
2020-03-28 20:39:19 +0100

    Add mailer -fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,12 @@
 class Contact < MailForm::Base
     attribute :name,      :validate => true
     attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-    attribute :subject,   :validate => true
     attribute :message,   :validate => true
     attribute :nickname,  :captcha => true
 
     def headers
     {
-      :subject => subject,
+      :subject => "Message du site",
       :to => "triki.nizar@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
