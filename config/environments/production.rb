c7789959bd22285e95ba6d4c0cb11552316b7025
Nizar Triki <triki.nizar@gmail.com>
2020-03-29 00:53:17 +0100

    Add mailer -fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,21 +98,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  class IncomingMailsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-
-  def create
-    Rails.logger.log params[:envelope][:to] # print the to field to the logs
-    Rails.logger.log params[:subject] # print the subject to the logs
-    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
-    Rails.logger.log params[:html] # print the html decoded body to the logs if present
-    Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
-
-    # Do some other stuff with the mail message
-
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
-  end
-end
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'artemipharma.net',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
 
 
 end
